c7757c36827c9cffadb4c389cdc3095ef60ff501
youbin <you8inpark@gmail.com>
2023-03-04 20:12:25 +0900

    :sparkles: feature add asset group function

--- src/components/AssetAllocation/AssetGroupsAdd/AssetGroup/index.jsx
+++ src/components/AssetAllocation/AssetGroupsAdd/AssetGroup/index.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { setAlloc } from "../../../../store/modules/alloc";
 import styled from "styled-components";
 import { IoIosArrowUp, IoIosArrowDown } from "react-icons/io";
 import { BsPercent } from "react-icons/bs";
@@ -7,15 +9,18 @@ import DropDown from "../../DropDown";
 import Button from "../../../Button";
 import { assetGroupsList } from "../../../../constant/assetgroups";
 
-const AssetGroup = ({ handleAdd, removeAsset }) => {
+const AssetGroup = ({ handleAsset, removeAsset }) => {
   const [isDropDown, setIsDropDown] = useState(false);
-
-  // useEffect(() => {
-  //   localStorage.getItem("assetGroup");
-  // }, []);
+  const dispatch = useDispatch();
+  const { asset } = useSelector((state) => state.alloc);
 
   const handleDropDown = () => {
     setIsDropDown(!isDropDown);
+  };
+
+  const selectAsset = (e) => {
+    let asset = e.target.value;
+    dispatch(setAlloc({ type: "asset", value: asset }));
   };
 
   return (
@@ -26,19 +31,26 @@ const AssetGroup = ({ handleAdd, removeAsset }) => {
         icon={isDropDown ? <IoIosArrowUp /> : <IoIosArrowDown />}
         onClick={handleDropDown}
         setIsDropDown={setIsDropDown}
+        value={asset}
       />
       {isDropDown && (
         <DropDown
           setIsDropDown={setIsDropDown}
           dropDownList={assetGroupsList}
+          onClick={selectAsset}
         />
       )}
       <h3 className="percent">비중</h3>
-      <SelectBox icon={<BsPercent />} />
+      <SelectBox
+        icon={<BsPercent />}
+        setIsDropDown={() => {
+          false;
+        }}
+      />
       <p>0 ~ 100 까지 입력할 수 있습니다.</p>
       <div className="button-wrapper">
         <Button title="삭제하기" color="black" onClick={removeAsset} />
-        <Button title="추가하기" color="orange" onClick={handleAdd} />
+        <Button title="추가하기" color="orange" onClick={handleAsset} />
       </div>
     </AssetGroupContainer>
   );
--- src/components/AssetAllocation/AssetGroupsAdd/index.jsx
+++ src/components/AssetAllocation/AssetGroupsAdd/index.jsx
@@ -1,38 +1,36 @@
 import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { setAlloc } from "../../../store/modules/alloc";
 import styled from "styled-components";
 import AssetGroup from "./AssetGroup";
 import Button from "../../Button";
-import SelectBox from "../SelectBox";
-import DropDown from "../DropDown";
 
 const ASSET_GROUP = localStorage.getItem("assetGroup");
+const IS_ASSET = localStorage.getItem("isAsset");
 
 const AssetGroupsAdd = () => {
   const [add, setAdd] = useState(Number(ASSET_GROUP));
-  const [isAdd, setIsAdd] = useState(false);
+  const [isAsset, setIsAsset] = useState(false);
+  const [group, setGroup] = useState([]);
+  const dispatch = useDispatch();
+  const { assetsGroup } = useSelector((state) => state.alloc);
 
-  // useEffect(() => {
-  // ASSET_GROUP ? setIsAdd(false) : setIsAdd(true);
-  // }, [add]);
-
-  // console.log("자산군개수추가", add);
-  // console.log("로컬 자산 수", ASSET_GROUP);
-  console.log("자산 수가 0개면 false", isAdd);
-
-  const handleAdd = () => {
-    setIsAdd(true);
+  const handleAsset = () => {
+    setIsAsset(true);
+    localStorage.setItem("isAsset", true);
   };
 
   const removeAsset = () => {
-    setIsAdd(false);
+    setIsAsset(false);
+    localStorage.removeItem("isAsset");
   };
 
   return (
     <AssetGroupsAddContainer>
-      {isAdd ? (
-        <AssetGroup handleAdd={handleAdd} removeAsset={removeAsset} />
+      {localStorage.getItem("isAsset") || isAsset ? (
+        <AssetGroup handleAdd={handleAsset} removeAsset={removeAsset} />
       ) : (
-        <Button onClick={handleAdd} title="추가하기" color="orange" />
+        <Button onClick={handleAsset} title="추가하기" color="orange" />
       )}
     </AssetGroupsAddContainer>
   );
