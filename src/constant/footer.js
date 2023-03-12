fe3f0093f1452b761a9acf43eca76e5c39110f0f
youbin <you8inpark@gmail.com>
2023-03-12 23:22:16 +0900

    ⚡refactor footer link list to constant

---
+++ src/constant/footer.js
@@ -0,0 +1,17 @@
+export const footerList = [
+  {
+    className: "cafe-link",
+    link: "https://cafe.naver.com/quantus",
+    title: "공식 카페 바로라기",
+  },
+  {
+    className: "link",
+    link: "https://www.quantus.kr/service/useservice",
+    title: "개인정보처리방침",
+  },
+  {
+    className: "link",
+    link: "https://www.quantus.kr/service",
+    title: "이용약관",
+  },
+];
--- src/pages/Any/index.jsx
+++ src/pages/Any/index.jsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 const index = () => {
   return (
     <Positioner>
-      <AnyContainer></AnyContainer>
+      <AnyContainer />
     </Positioner>
   );
 };
@@ -17,7 +17,6 @@ const AnyContainer = styled.div`
   position: relative;
   height: 100%;
   min-height: 900px;
-  background: rgb(14, 14, 14);
 `;
 
 export default index;
